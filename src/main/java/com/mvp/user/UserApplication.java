c74f98757db33fa8ed341fb1187c16d4752053bd
vasanthpandian <vasanth2011@gmail.com>
2019-05-01 23:46:00 +0530

    Eureka Integration

--- src/main/java/com/mvp/user/UserApplication.java
+++ src/main/java/com/mvp/user/UserApplication.java
@@ -2,9 +2,11 @@ package com.mvp.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@SpringBootApplication
+@SpringBootApplication	
+@EnableEurekaClient
 public class UserApplication {
 
 	public static void main(String[] args) {
